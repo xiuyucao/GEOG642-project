25676f3c1d7a18afb8dd09a5dbb8e4b37c40cfc0
Xiuyu Cao <mattxcao@gmail.com>
2026-04-13 18:49:30 -0400

    Get all GEDI pairs

--- scripts/2_pair_gedi.R
+++ scripts/2_pair_gedi.R
@@ -4,6 +4,17 @@ library(sf)
 
 
 # ---------------------------------- Functions --------------------------------- #
+# Load GEDI L2 and L4A from time 0, 1, 2 and filter to fire_buffer
+load_gedi <- function(prefix) {
+  lapply(c('0', '1', '2'), function(t) {
+    read_rds(file.path(gedi_folder, paste0(prefix, t, '.rds'))) %>%
+      st_as_sf(coords = c('lon', 'lat'), crs = 4326) %>%
+      st_transform(crs = 5070) %>%
+      filter(as.vector(st_within(., fire_buffer, sparse = FALSE)))
+  }) %>% setNames(c('0', '1', '2'))
+}
+
+# Get GEDI pairs based on the filters
 get_pairs <- function(sf_a, sf_b, max_dist = 45, max_elev_diff = 10) {
   nn <- st_nearest_feature(sf_a, sf_b)
   dist <- as.numeric(st_distance(sf_a, sf_b[nn, ], by_element = TRUE))
@@ -16,11 +27,11 @@ get_pairs <- function(sf_a, sf_b, max_dist = 45, max_elev_diff = 10) {
     filter(dist <= max_dist, elev_diff <= max_elev_diff)
 }
 
+# Get all variables from the original data for the pairs
 join_pair_attrs <- function(pairs, sf_a, sf_b, time_a, time_b) {
   attrs_a <- sf_a %>% st_drop_geometry() %>% rename_with(~ paste0(., '_', time_a), -shot_number)
   attrs_b <- sf_b %>% st_drop_geometry() %>% rename_with(~ paste0(., '_', time_b), -shot_number)
   
-  # Compute mean location between paired shots
   coords_a <- st_coordinates(sf_a)[match(pairs$shot_a, sf_a$shot_number), ]
   coords_b <- st_coordinates(sf_b)[match(pairs$shot_b, sf_b$shot_number), ]
   mean_coords <- (coords_a + coords_b) / 2
@@ -32,22 +43,23 @@ join_pair_attrs <- function(pairs, sf_a, sf_b, time_a, time_b) {
     st_as_sf(coords = c('x', 'y'), crs = 5070)
 }
 
+# Get the fire/control zone and burn severity information for each pair
 add_fire_attrs <- function(pairs_sf, fire, control, burn_severity_binary) {
-  in_fire <- as.vector(st_within(pairs_sf, fire, sparse = FALSE))
+  in_fire    <- as.vector(st_within(pairs_sf, fire,    sparse = FALSE))
   in_control <- as.vector(st_within(pairs_sf, control, sparse = FALSE))
-  severity <- extract(burn_severity_binary, vect(pairs_sf))[[2]]
+  severity   <- extract(burn_severity_binary, vect(pairs_sf))[[2]]
   
   pairs_sf %>%
-    mutate(zone = case_when(in_fire ~ 'fire', in_control ~ 'control', TRUE ~ NA_character_)) %>%
-    mutate(burn_severity = severity)
+    mutate(zone = case_when(in_fire ~ 'fire', in_control ~ 'control', TRUE ~ NA_character_),
+           burn_severity = severity)
 }
 
 
 # ------------------------------------ Paths ----------------------------------- #
-gedi_folder <- 'data/input/gedi'
+gedi_folder          <- 'data/input/gedi'
 fire_perimeters_file <- 'data/input/fires/ca_2020_fires.shp'
-burn_severity_file <- 'data/raw/fire/burn_severity.tif'
-output_folder <- 'data/intermediate'
+burn_severity_file   <- 'data/raw/fire/burn_severity.tif'
+output_folder        <- 'data/intermediate'
 
 
 # ---------------------------------- Get Fire ---------------------------------- #
@@ -65,29 +77,23 @@ burn_severity_binary <- classify(burn_severity,
 
 
 # ------------------------------- Get GEDI Pairs ------------------------------- #
-# Load GEDI L2 from time 0, 1, 2
-l2_sf <- lapply(c('0', '1', '2'), function(t) {
-  read_rds(file.path(gedi_folder, paste0('gedi_l2_', t, '.rds'))) %>%
-    st_as_sf(coords = c('lon', 'lat'), crs = 4326) %>%
-    st_transform(crs = 5070)
-})
-names(l2_sf) <- c('0', '1', '2')
-
-# Keep only shots within fire_buffer
-l2_sf <- lapply(l2_sf, function(sf) {
-  sf[st_within(sf, fire_buffer, sparse = FALSE), ]
-})
+l2_sf  <- load_gedi('gedi_l2_')
+l4a_sf <- load_gedi('gedi_l4a_')
 
 # Get pairs for each time combination
-pairs_01 <- get_pairs(l2_sf[['0']], l2_sf[['1']]) %>%
-  join_pair_attrs(l2_sf[['0']], l2_sf[['1']], '0', '1') %>%
-  add_fire_attrs(fire, control, burn_severity_binary) %>%
-  write_rds(file.path(output_folder, 'gedi_pairs_01.rds'))
-pairs_12 <- get_pairs(l2_sf[['1']], l2_sf[['2']]) %>%
-  join_pair_attrs(l2_sf[['1']], l2_sf[['2']], '1', '2') %>%
-  add_fire_attrs(fire, control, burn_severity_binary) %>%
-  write_rds(file.path(output_folder, 'gedi_pairs_12.rds'))
-pairs_02 <- get_pairs(l2_sf[['0']], l2_sf[['2']]) %>%
-  join_pair_attrs(l2_sf[['0']], l2_sf[['2']], '0', '2') %>%
-  add_fire_attrs(fire, control, burn_severity_binary) %>%
-  write_rds(file.path(output_folder, 'gedi_pairs_02.rds'))
+for (combo in list(c('0', '1'), c('1', '2'), c('0', '2'))) {
+  t_a <- combo[1]; t_b <- combo[2]
+  suffix <- paste0(t_a, t_b)
+  
+  # L2 pairs
+  get_pairs(l2_sf[[t_a]], l2_sf[[t_b]]) %>%
+    join_pair_attrs(l2_sf[[t_a]], l2_sf[[t_b]], t_a, t_b) %>%
+    add_fire_attrs(fire, control, burn_severity_binary) %>%
+    write_rds(file.path(output_folder, paste0('l2_pairs_', suffix, '.rds')))
+  
+  # L4A pairs
+  get_pairs(l4a_sf[[t_a]], l4a_sf[[t_b]]) %>%
+    join_pair_attrs(l4a_sf[[t_a]], l4a_sf[[t_b]], t_a, t_b) %>%
+    add_fire_attrs(fire, control, burn_severity_binary) %>%
+    write_rds(file.path(output_folder, paste0('l4a_pairs_', suffix, '.rds')))
+}
